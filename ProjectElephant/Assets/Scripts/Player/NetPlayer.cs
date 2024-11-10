42eeb30e69c20839f7af66ba0f68d7af4c2a49b4
Wardergrip <42802496+Wardergrip@users.noreply.github.com>
2024-11-10 22:08:40 +0100

    Simplified NetPlayer with pawn

--- ProjectElephant/Assets/Scripts/Player/NetPlayer.cs
+++ ProjectElephant/Assets/Scripts/Player/NetPlayer.cs
@@ -5,6 +5,50 @@ namespace Elephant
 {
     public class NetPlayer : NetworkBehaviour
     {
-        [SerializeField] private NetworkBehaviour _pawnToSpawn;
+        [SerializeField] private NetworkBehaviour _pawn;
+
+        [Client]
+        private void OnEnable()
+        {
+            if (GameCamera.Active != null)
+            {
+                GameCamera.Active.ToFollow = _pawn.transform;
+            }
+        }
+
+        private void Update()
+        {
+            if (isClient)
+            {
+                ClientUpdate();
+            }
+        }
+
+        [Client]
+        private void ClientUpdate()
+        {
+            if (Input.GetKey(KeyCode.W)) 
+            {
+                RPC_MoveInput(Vector3.forward);
+            }
+            if (Input.GetKey(KeyCode.S)) 
+            {
+                RPC_MoveInput(Vector3.back);
+            }
+            if (Input.GetKey(KeyCode.A)) 
+            {
+                RPC_MoveInput(Vector3.left);
+            }
+            if (Input.GetKey(KeyCode.D)) 
+            {
+                RPC_MoveInput(Vector3.right);
+            }
+        }
+
+        [Command]
+        private void RPC_MoveInput(Vector3 direction)
+        {
+            transform.position += direction * Time.deltaTime;
+        }
     }
 }
